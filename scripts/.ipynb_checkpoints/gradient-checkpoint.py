12b2619cd7dd0fb3f073735ef2f08bb7c5370f75
ogimgio <ogimgamers@gmail.com>
2021-10-16 16:02:29 +0200

    we have to clean the dataset

--- scripts/.ipynb_checkpoints/gradient-checkpoint.py
+++ scripts/.ipynb_checkpoints/gradient-checkpoint.py
@@ -4,7 +4,7 @@ from costs import calculate_mse
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
     err = y - tx.dot(w)
-    grad = -tx.T.dot(err) / len(err)
+    grad = (-1/len(err))*tx.T.dot(err)
     return grad, err
 
 
