d1714ca9f90fa13be06e27666e80a050e81b5e5f
Stefan <stefanigescu@gmail.com>
2021-11-01 12:22:04 +0100

    update code folder

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ So go at the following two links and donwload the files:
 - https://drive.google.com/file/d/1GlgHTsIrML1Mls04R8IQ8E-wWbz4YHF5/view?usp=sharing
 - https://drive.google.com/file/d/1VrkRo8mkOWQokTq4rRU0vkp1atgsX-rj/view?usp=sharing
 
-Then, move them to the data folder. This way our script will ensure it gets the right data from the right place.
+Place the test.csv and train.csv files into the /code folder.
 
-To run the code, make sure numpy and matplotlib are installed. Go in run.py and run the respective python file. You will receive an output.csv file in the same directory of the run.py file, with all the prediction made using the most accurate model.
+To run the code, make sure numpy and matplotlib are installed.
+
+Go into the /code folder and execute the run.py file. You will receive an output_logistic.csv file in the same directory of the run.py file, with all the prediction made using the most accurate model.
 
 The implementations.py file contains all the respective machine learning models, with the addition of the polynomial expansion creation and the cross-validation.
+
 The proj1_helpers.py contains helping function as to load the data, predict the output and do the submission, as well as preprocessing functions as standardization and outlier removal.
--- code/run.py
+++ code/run.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import collections
 from proj1_helpers import *
 from implementations import *
+print('Loading data:')
 DATA_TRAIN_PATH = 'train.csv'
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
@@ -23,7 +24,7 @@ tX_list,y_list=split_data(tX, y)
 
 
 # Logistic Cross Validation - Best degree, gamma, and if interaction is needed
-print('Logistic Cross Validation:')
+print('\nLogistic Cross Validation:')
 degrees=np.arange(3)
 k_fold=4
 max_iters=100
@@ -50,52 +51,15 @@ for h,x in enumerate(tX_list):
     print(f'\t\ttX_{h} Best degree logistic: {D_best_logistic}, best gamma logistic:{gamma_best}, interaction:{interaction_logistic}')
     tX_results_list_logistic.append({'D_best':D_best_logistic,'gamma_best':gamma_best,'interaction':interaction_logistic})
 
-# Cross Validation Ridge Regression - Best degree, lambda_m, and if interaction is needed
-print('Ridge Regression Cross Validation:')
-degrees=np.arange(3)
-lambdas=np.logspace(-10,0,10)
-k_fold=4
-interactions=[False,True]
-tX_results_list_ridge=[]
-for h,x in enumerate(tX_list):
-    rmse_tr_list=np.zeros((len(degrees),len(lambdas),len(interactions)))
-    rmse_te_list=np.zeros((len(degrees),len(lambdas),len(interactions)))
-    print(f'\t- tX_{h} iteration')
-    for i,D in enumerate(degrees):
-        print(f'\t\tDegree: {D}')
-        for k,interaction in enumerate(interactions):
-            phi_x=build_poly(x, D, interaction)
-            for j,lambda_ in enumerate(lambdas):
-                #compute loss with cross-validation
-                rmse_tr, rmse_te=apply_cross_validation(y_list[h],phi_x,k_fold,D,lambda_,1)
-                rmse_tr_list[i,j,k]=rmse_tr
-                rmse_te_list[i,j,k]=rmse_te
-    D_best_index,lambda_best_index,interaction_index=np.unravel_index(np.argmin(rmse_te_list),rmse_te_list.shape)
-    D_best_ridge=degrees[D_best_index]
-    lambda_best_ridge=lambdas[lambda_best_index]
-    interaction_ridge=interactions[interaction_index]
-    print(f'\t\ttX_{h} Best degree ridge:{D_best_ridge}, best lambda_ ridge:{lambda_best_ridge}, interactions: {interaction_ridge}')
-    tX_results_list_ridge.append({'D_best':D_best_ridge,'lambda_best':lambda_best_ridge,'interaction':interaction_ridge})
-
-# Ridge Regression and Logistic accuracy on train data
-weights_ridge=[]
+#Logistic accuracy on train data
 weights_logistic=[]
+print('\nLogistic accuracy on train data:')
 for i,x in enumerate(tX_list):
-    D_best_ridge=tX_results_list_ridge[i]['D_best']
-    interaction_ridge=tX_results_list_ridge[i]['interaction']
-    lambda_best_ridge=tX_results_list_ridge[i]['lambda_best']
-
     D_best_logistic=tX_results_list_logistic[i]['D_best']
     interaction_logistic=tX_results_list_logistic[i]['interaction']
     gamma_best=tX_results_list_logistic[i]['gamma_best']
     #Re-transform y data
     y_act=y_list[i]*2-1
-    #Ridge
-    phi_x_ridge=build_poly(x,D_best_ridge,interaction_ridge)
-    w_ridge,_=ridge_regression(y_list[i],phi_x_ridge,lambda_best_ridge)
-    weights_ridge.append(w_ridge)
-    y_pred_ridge=predict_labels(w_ridge,phi_x_ridge)
-    accuracy_ridge=accuracy(y_pred_ridge,y_act)
     #Logistic
     phi_x_logistic=build_poly(x, D_best_logistic, interaction_logistic)
     w_initial=np.zeros(phi_x_logistic.shape[1])
@@ -104,7 +68,7 @@ for i,x in enumerate(tX_list):
     weights_logistic.append(w_logistic)
     y_pred_logistic=predict_labels(w_logistic,phi_x_logistic,logistic=True)
     accuracy_logistic=accuracy(y_pred_logistic,y_act)
-    print(f'tX_{i} -- Accuracy ridge:{accuracy_ridge} Accuracy logistic:{accuracy_logistic}')
+    print(f'tX_{i} -- Accuracy logistic:{accuracy_logistic}')
 
 # Generate predictions and save ouput in csv format for submission:
 DATA_TEST_PATH = 'test.csv'
@@ -115,19 +79,6 @@ w_dummy=np.zeros(tX_test.shape[1])
 y_pred=predict_labels(w_dummy,tX_test)
 tX_test_list,_=split_data(tX_test,y_pred,ignore_y=True,print_=False)
 
-#Ridge submission
-for i,x in enumerate(tX_results_list_ridge):
-    D_best_ridge=tX_results_list_ridge[i]['D_best']
-    interaction_ridge=tX_results_list_ridge[i]['interaction']
-    lambda_best_ridge=tX_results_list_ridge[i]['lambda_best']
-
-    tX_test_ridge=build_poly(tX_test_list[i],D_best_ridge,interaction_ridge)
-    weights=weights_ridge[i]
-    y_pred[tX_test[:,22]==i] = predict_labels(weights, tX_test_ridge)
-
-OUTPUT_PATH = 'output_ridge.csv' # TODO: fill in desired name of output file for submission
-create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
-
 #Logistic submission
 for i,x in enumerate(tX_results_list_logistic):
     D_best_logistic=tX_results_list_logistic[i]['D_best']
@@ -137,6 +88,6 @@ for i,x in enumerate(tX_results_list_logistic):
     tX_test_logistic=build_poly(tX_test_list[i],D_best_logistic,interaction_logistic)
     weights=weights_logistic[i]
     y_pred[tX_test[:,22]==i]=predict_labels(weights,tX_test_logistic, logistic=True)
-    
 OUTPUT_PATH = 'output_logistic.csv' # TODO: fill in desired name of output file for submission
 create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
+print('\nSubmission output_logistic.csv created.') 
